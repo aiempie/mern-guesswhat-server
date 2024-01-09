369c793b00df69d0f464fc7a6117d386d2550923
khanhtn <khanhtnfptdn@gmail.com>
2024-01-09 14:16:19 +0700

    fix admin auth

--- routers/authRoute/authRoute.js
+++ routers/authRoute/authRoute.js
@@ -11,7 +11,7 @@ const router = express.Router();
 router.post("/register", createUser);
 router.post("/login", loginUser);
 router.get("/loaduser", verifyToken, loadUser);
-router.get("/:id", getUserById);
 router.get("/loadadmin", verifyToken, checkMod, loadUser);
+router.get("/:id", getUserById);
 
 module.exports = router;
